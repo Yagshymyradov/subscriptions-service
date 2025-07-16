e4a8835af6e3054b5dbe957f4e57060557b722e9
Yagshymyradov <yagshymyradovgaygysyz69@gmail.com>
2025-07-16 16:55:22 +0500

    set config env

--- cmd/subscriptions-service/main.go
+++ cmd/subscriptions-service/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/Yagshymyradov/subscriptions-service/internal/config"
+)
 
 func main(){
-	log.Println("subscription service started")
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	log.Printf("Starting service on port %s", cfg.HTTP.Port)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	HTTP HTTPConfig
+	DB DBConfig
+	Log LogConfig
+}
+
+type HTTPConfig struct{
+	Port string `mapstructure:"port"`
+}
+
+type DBConfig struct {
+	DSN string `mapstructure:"dsn"`
+	MaxOpenConns int `mapstructure:"max_open_conns"`
+	MaxIdleConns int `mapstructure:"max_idle_conns"`
+	ConnMaxLifetimeSeconds int `mapstructure:"conn_max_lifetime_seconds"`
+}
+
+type LogConfig struct {
+	Level string `mapstructure:"level"`
+}
+
+func Load() (*Config, error) {
+	v := viper.New()
+
+	v.SetEnvPrefix("APP")
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	v.AutomaticEnv()
+
+	v.SetDefault("http.port", "8080")
+	v.SetDefault("db.max_open_conns", 10)
+	v.SetDefault("db.max_idle_conns", 5)
+	v.SetDefault("db.conn_max_lifetime_seconds", 300)
+	v.SetDefault("log.level", "info")
+
+	v.SetConfigName("config")
+	v.AddConfigPath(".")
+	_ = v.ReadInConfig()
+
+	var cfg Config
+	if err := v.Unmarshal(&cfg); err !=  nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
